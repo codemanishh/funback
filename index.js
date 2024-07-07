ff06d708f86d536e5fbb0a1be404bb8a12d6f9df
manish <rrew1212@gmail.com>
2024-07-07 12:46:32 +0530

    dd

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = 4000
 app.get('/', (req, res) => {
     res.send('Hello singhh')
 })
+app.get('/codemanishh', (req, res) => {
+    res.send('Hello mansih jiii')
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
